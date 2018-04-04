74f47bff531366da18cd6232cb924043d879c2c1
Raghu Betina <raghu@firstdraft.com>
2018-04-04 15:22:07 +0000

    Direct association between users and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   has_many   :sent_requests,
              :class_name => "Request",
              :foreign_key => "sender_id",
